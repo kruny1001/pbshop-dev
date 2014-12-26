bc69647ede74c190d5c33884110725e07f9e7a2b
Kevin <kruny1001@gmail.com>
2014-12-26 02:38:06 -0800

    tj hair

--- public/modules/tj-hair/controllers/tj-main.client.controller.js
+++ public/modules/tj-hair/controllers/tj-main.client.controller.js
@@ -5,18 +5,44 @@ angular.module('tj-hair').controller('TjMainController', ['$scope','$timeout',
 		// Tj main controller logic
 		// ...
 
-		$scope.addAvatar = function(parentID, position){
+		$scope.map = { center: { latitude: 44.970697, longitude: -93.2614785 }, zoom:11 };
+		$scope.marker = {
+			id: 0,
+			coords: {
+				latitude: 45.007025,
+				longitude: -93.3974526
+			},
+			options: { draggable: false },
+			events: {
+				dragend: function (marker, eventName, args) {
+					$log.log('marker dragend');
+					var lat = marker.getPosition().lat();
+					var lon = marker.getPosition().lng();
+					$log.log(lat);
+					$log.log(lon);
+
+					$scope.marker.options = {
+						draggable: true,
+						labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+						labelAnchor: "100 0",
+						labelClass: "marker-labels"
+					};
+				}
+			}
+		};
+
+		// add Avatar
+		$scope.addSVG = function(parentID, position){
 			var parentSvg = Snap('#'+parentID);
 			var pin = Snap.load("modules/tj-hair/img/people1.svg", function(data){
 				var group = data;
-
 				/*
 				var t = new Snap.Matrix();
 				t.translate(position.x, position.y);
 				t.scale(1);
 				group.transform(t);
 				group.attr({id:position.id, cursor: 'pointer'});
-				 */
+
 				var move = function(dx,dy) {
 					this.attr({
 						transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
@@ -29,7 +55,7 @@ angular.module('tj-hair').controller('TjMainController', ['$scope','$timeout',
 					console.log('finished dragging');
 					console.log(this);
 				}
-
+				 */
 				//group.drag(move, start, stop);
 				parentSvg.append(group);
 				//$scope.addedObject.push(group.node.id);
@@ -40,10 +66,11 @@ angular.module('tj-hair').controller('TjMainController', ['$scope','$timeout',
 		var backSvg = Snap('#backGroundSvg');
 		Snap.load("modules/tj-hair/img/building1.svg", function(data) {
 			backSvg.append(data);
-
+			var circlePinPosition = {x:'443' , y:'299', id:'circlePin1'};
+			$scope.addSVG('backGroundSvg', circlePinPosition);
 			$timeout(function() {
-				var circlePinPosition = {x:'443' , y:'299', id:'circlePin1'};
-				$scope.addAvatar('backGroundSvg', circlePinPosition);
+
+				TweenLite.to('#p1 #whole', 2, {opacity:1, scale:0.7, x:50, y:50})
 				/*
 				TweenMax.to('#backGroundSvg', 55, {scrollTo: {y: 0, x: 2000}, ease: Power2.easeInOut});
 
@@ -68,7 +95,7 @@ angular.module('tj-hair').controller('TjMainController', ['$scope','$timeout',
 				$scope.selectN = false;
 				TweenMax.staggerFrom(".frontNavBtn", 2, {scale:0.5, opacity:0, delay:0.5, ease:Elastic.easeOut, force3D:true}, 0.2);
 				*/
-			},1500);
+			},2000);
 		});
 
 		var cutSvg = Snap('#cut');
