e9a62fb62a382d2c02c64bec723837f23515f9d0
Kevin <kruny1001@gmail.com>
2015-03-24 23:34:14 -0600

    lms d2l-classes

--- result.js
+++ result.js
@@ -6,17 +6,21 @@
 	hwInfo=
 		{
 			content=,
-			title=v1 Assignment Add On,
+			//Assignment name
+            title=v1 Assignment Add On,
 			desc=version 1 Add on,
-			percent=1,
-			_id=5511e79fe96a4d0b00cb58b6,
-			created=2015-03-24T22:39:27.406Z,
-			totalGrade=200,
-			dDate=2015-04-23T05:00:00.000Z,
+
+            percent=1,
+			//
+            _id=5511e79fe96a4d0b00cb58b6,
+			//createdDate
+            created=2015-03-24T22:39:27.406Z,
+			//totalgrade
+            totalGrade=200,
+			//due date
+            dDate=2015-04-23T05:00:00.000Z,
 			__v=0,
-
 			class={_id=55100a8cd30b150b0002e772, created=2015-03-23T12:43:56.108Z, name=Accelerated Computer Science Fundamentals, prefix=CSC-601, __v=0, user=53bc47cd10a5bf0800bf940f},
-
 			gdocId=1XpjMDFnJGuoGXfsn--eUT0GBYjjEBxBL_tm2bC3RHfc,
 	    user={_id=53bc47cd10a5bf0800bf940f, email=kruny1001@gmail.com, displayName=Kevin Son}
     },
