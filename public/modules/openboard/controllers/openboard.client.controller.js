a7f072f7110ed24db444fa136a172a9e38737e2d
Kevin Son <kruny1001@gmail.com>
2015-03-28 22:18:21 -0500

    getHTTP interaction2

--- public/modules/openboard/controllers/openboard.client.controller.js
+++ public/modules/openboard/controllers/openboard.client.controller.js
@@ -19,7 +19,7 @@ function OpenboardController($scope, $mdDialog, $window, $http, Authentication, 
 	$scope.classesCopy = [].concat($scope.classes);
 
 
-	https://script.google.com/macros/s/AKfycbzoXxZDgzjLOJdqGUGYCWSpIT7n2sHyvnIo2W7E5jmXI_2sryj3/exec?docId=1S1cucFhz96cGRHvXzgGqxuptNR-NgWKpIwI55RB67NU&userId=kruny1001&title=3_28%20test1&dDate=2015-04-01T05:00:00.000Z&userIdRef=54cfa0c6d5940f501bf796e7&classId=551710ba93a97a044e276fac
+	//https://script.google.com/macros/s/AKfycbzoXxZDgzjLOJdqGUGYCWSpIT7n2sHyvnIo2W7E5jmXI_2sryj3/exec?docId=1S1cucFhz96cGRHvXzgGqxuptNR-NgWKpIwI55RB67NU&userId=kruny1001&title=3_28%20test1&dDate=2015-04-01T05:00:00.000Z&userIdRef=54cfa0c6d5940f501bf796e7&classId=551710ba93a97a044e276fac
 	$scope.linkHW = function(hw){
 		var AppScriptAPI = 'https://script.google.com/macros/s/AKfycbzoXxZDgzjLOJdqGUGYCWSpIT7n2sHyvnIo2W7E5jmXI_2sryj3/exec?';
 		var param = 'docId='+hw.gdocId+
