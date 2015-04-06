5c570f94a1f3a53171a91e633bedeea03110cfb7
Kevin Son <kruny1001@gmail.com>
2015-04-05 22:21:00 -0500

    side nav

--- public/dist/application.js
+++ public/dist/application.js
@@ -398,12 +398,59 @@ angular.module('core')
         };
         $scope.currentState = function(){};
         $scope.onchangeRoute = function(){};
+      var scrollTo = function(){
+        console.log('scrollTo funciton');
+      };
+
+      $scope.tiles = buildGridModel({
+        icon : "avatar:svg-",
+        title: "Svg-",
+        background: ""
+      });
+      function buildGridModel(tileTmpl){
+        var it, results = [ ];
+        for (var j=0; j<6; j++) {
+          it = angular.extend({},tileTmpl);
+          it.icon  = it.icon + (j+1);
+          //it.title = it.title + (j+1);
+          it.span  = { row : "1", col : "1" };
+          switch(j+1) {
+            case 1:
+              it.background = "red";
+              it.title = "Profile";
+              it.span.row = it.span.col = 2;
+              break;
+            case 2: it.background = "green";  it.span.col = 2;  it.title = "Class";     break;
+            case 3: it.background = "darkBlue";  it.span.col = 2; it.title = "Notifications";   break;
+            case 4:
+              it.title = "Class";
+              it.background = "blue";
+              it.span.col = 2;
+              break;
+            case 5:
+              it.background = "yellow";
+              it.span.row = it.span.col = 1;
+              break;
+            case 6: it.background = "pink";          break;
+            //case 7: it.background = "darkBlue";      break;
+            //case 8: it.background = "purple";        break;
+            //case 9: it.background = "deepBlue";      break;
+            //case 10: it.background = "lightPurple";  break;
+            //case 11: it.background = "yellow";       break;
+          }
+          results.push(it);
+        }
+        return results;
+      }
+
+
 
         $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
             console.log('closed');
             if(toState.name === "openboard"){
                 $scope.title = "Getting Started"
                 $scope.subTitle = "Tutorial"
+
             }
             else if(toState.name === "mean-home")
             {
@@ -2745,6 +2792,32 @@ angular.module('etc').directive('colorPicker', [
 		};
 
 		function ColorPickerCtrl($scope) {
+			this.items=[
+				{imgSrc:"modules/etc/img/1.jpg", title:"Color 1"},
+				{imgSrc:"modules/etc/img/2.jpg", title:"Color 2"},
+				{imgSrc:"modules/etc/img/3.jpg", title:"Color 3"},
+				{imgSrc:"modules/etc/img/4.jpg", title:"Color 4"},
+				{imgSrc:"modules/etc/img/5.jpg", title:"Color 5"},
+				{imgSrc:"modules/etc/img/6.jpg", title:"Color 6"},
+				{imgSrc:"modules/etc/img/7.jpg", title:"Color 7"},
+				{imgSrc:"modules/etc/img/8.jpg", title:"Color 8"},
+				{imgSrc:"modules/etc/img/9.jpg", title:"Color 9"},
+				{imgSrc:"modules/etc/img/10.jpg", title:"Color 10"},
+				{imgSrc:"modules/etc/img/11.jpg", title:"Color 11"},
+				{imgSrc:"modules/etc/img/12.jpg", title:"Color 12"},
+				{imgSrc:"modules/etc/img/13.jpg", title:"Color 13"},
+				{imgSrc:"modules/etc/img/14.jpg", title:"Color 14"},
+				{imgSrc:"modules/etc/img/15.jpg", title:"Color 15"},
+				{imgSrc:"modules/etc/img/16.jpg", title:"Color 16"},
+			];
+
+			this.coverSelected = function(event) {
+				console.log("selected");
+				//console.log(event);
+				console.log(event.path[2].outerHTML)
+				TweenLite.to(event.path[2].outerHTML, 1, {display:"block"});
+			}
+
 			this.tiles = buildGridModel({
 				icon : "avatar:svg-",
 				title: "Svg-",
@@ -2797,6 +2870,26 @@ angular.module('etc').directive('colorPicker', [
 				return results;
 			}
 		}
+	}
+]);
+'use strict';
+
+angular.module('etc').directive('productDetail', [
+	function() {
+		ProductDetailCtrl.$inject = ["$scope"];
+		return {
+			templateUrl: 'modules/etc/directives/template/product-detail.html',
+			restrict: 'E',
+			controller: ProductDetailCtrl,
+			controllerAs: 'vm',
+			link: function postLink(scope, element, attrs) {
+
+			}
+		};
+
+		function ProductDetailCtrl($scope) {
+
+		};
 	}
 ]);
 'use strict';
